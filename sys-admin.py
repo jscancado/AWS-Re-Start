8a85f9d4d6ff9379889ff3f8baa2ecd40d3e4b2a
JCANCADO <jcancado@ford.com>
2023-03-03 10:48:32 +1100

    message

---
+++ sys-admin.py
@@ -0,0 +1,30 @@
+#System Admin with Python
+
+import os
+import subprocess
+
+#Using os.system
+os.system("ls")
+
+#Using subprocess.run
+subprocess.run(["ls"])
+
+#Using subprocess.run with two arguments
+subprocess.run(["ls","-l"])
+
+#Using subprocess.run with three arguments
+subprocess.run(["ls","-l","README.md"])
+
+
+#Retrieving system information:
+command="uname"
+commandArgument="-a"
+print(f'Gathering system information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
+
+
+#Retrieving information about disk space:
+command="ps"
+commandArgument="-x"
+print(f'Gathering active process information with command: {command} {commandArgument}')
+subprocess.run([command,commandArgument])
